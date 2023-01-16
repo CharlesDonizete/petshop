4c2193bcef814ded8614b07680940825be0ae9de
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-01-16 18:36:37 -0300

    Master Pages

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,16 @@
-import { NgModule } from '@angular/core';
+import { LoginPageComponent } from './pages/account/login-page/login-page.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'login',
+    component: LoginPageComponent,
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,6 +8,7 @@ import { LoginPageComponent } from './pages/account/login-page/login-page.compon
 import { PetsPageComponent } from './pages/account/pets-page/pets-page.component';
 import { ResetPasswordPageComponent } from './pages/account/reset-password-page/reset-password-page.component';
 import { SignupPageComponent } from './pages/account/signup-page/signup-page.component';
+import { FramePageComponent } from './pages/master/frame.page';
 import { CartPageComponent } from './pages/store/cart-page/cart-page.component';
 import { ProductsPageComponent } from './pages/store/products-page/products-page.component';
 
@@ -21,6 +22,7 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
     ProductsPageComponent,
     PetsPageComponent,
     CartPageComponent,
+    FramePageComponent,
   ],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
