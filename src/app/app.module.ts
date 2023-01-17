b20a594a646722d391e675855cda145b529b724b
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-01-16 21:07:14 -0300

    Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
@@ -24,7 +24,7 @@ import { ProductsPageComponent } from './pages/store/products-page/products-page
     CartPageComponent,
     FramePageComponent,
   ],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
