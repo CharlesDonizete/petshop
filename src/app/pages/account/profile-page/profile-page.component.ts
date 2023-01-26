434b335ba8a36c6fbbcdfe0791960e1dbd375338
CharlesDonizete <82051427+CharlesDonizete@users.noreply.github.com>
2023-01-25 21:30:01 -0300

    Edição de Perfil: Parte 1

--- src/app/pages/account/profile-page/profile-page.component.ts
+++ src/app/pages/account/profile-page/profile-page.component.ts
@@ -1,15 +1,60 @@
+import { CustomValidator } from 'src/app/validators/custom.validator';
+import { ToastrService } from 'ngx-toastr';
+import { Router } from '@angular/router';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 import { Component, OnInit } from '@angular/core';
+import { DataService } from 'src/app/services/data.service';
 
 @Component({
   selector: 'app-profile-page',
   templateUrl: './profile-page.component.html',
-  styleUrls: ['./profile-page.component.css']
+  styleUrls: ['./profile-page.component.css'],
 })
 export class ProfilePageComponent implements OnInit {
+  public form: FormGroup;
+  public busy = false;
 
-  constructor() { }
-
-  ngOnInit(): void {
+  constructor(
+    private router: Router,
+    private service: DataService,
+    private fb: FormBuilder,
+    private toastr: ToastrService
+  ) {
+    this.form = this.fb.group({
+      name: [
+        '',
+        Validators.compose([
+          Validators.minLength(3),
+          Validators.maxLength(80),
+          Validators.required,
+        ]),
+      ],
+      document: [{ value: '', disabled: true }],
+      email: [
+        '',
+        Validators.compose([
+          Validators.minLength(5),
+          Validators.maxLength(120),
+          Validators.required,
+          CustomValidator.EmailValidator,
+        ]),
+      ],
+    });
   }
 
+  ngOnInit(): void {
+    this.busy = true;
+    this.service.getProfile().subscribe(
+      (data: any) => {
+        this.busy = false;
+        this.form.controls['name'].setValue(data.name);
+        this.form.controls['document'].setValue(data.document);
+        this.form.controls['email'].setValue(data.email);
+      },
+      (err) => {
+        console.log(err);
+        this.busy = false;
+      }
+    );
+  }
 }
